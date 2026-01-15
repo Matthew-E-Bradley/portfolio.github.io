abfa76bb55406e03b49a7bed61601f6d802c6977
Matthew-E-Bradley <164899281+Matthew-E-Bradley@users.noreply.github.com>
2026-01-14 21:40:01 -0500

    vite js config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/portfolio/', // This ensures your images and CSS load from the subfolder
-  // ... rest of config
+  // 1. Base goes here (outside of plugins)
+  // Use '/' for your custom domain (matthew-bradley.com)
+  base: '/', 
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
+      // This allows you to use '@' to refer to your 'src' folder
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
