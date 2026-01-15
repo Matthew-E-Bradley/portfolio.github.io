0800af1c6b74f9aa50b028ba44df9f21a25922c5
Matthew-E-Bradley <164899281+Matthew-E-Bradley@users.noreply.github.com>
2026-01-14 20:38:28 -0500

    commit1

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/portfolio/', // This ensures your images and CSS load from the subfolder
+  // ... rest of config
   plugins: [
     vue(),
     vueDevTools(),
