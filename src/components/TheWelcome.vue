ac814c7d988c02ead9dd0693968a493bfda0d12c
Matthew-E-Bradley <164899281+Matthew-E-Bradley@users.noreply.github.com>
2026-01-17 22:05:06 -0500

    icons

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -7,7 +7,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 <template>
   <WelcomeItem>
     <template #icon>
-      <i class="fi fi-tr-customize-computer"></i>
+      <i class="fi fi-sr-customize-computer"></i>
     </template>
     <template #heading>Graphic Design</template>
 
@@ -16,7 +16,7 @@ Flyers, Logos, Banners, Social Media Posts, and more. I provide professional gra
 
   <WelcomeItem>
     <template #icon>
-      <i class="fi fi-tr-person-chalkboard"></i>
+      <i class="fi fi-sr-person-chalkboard"></i>
     </template>
     <template #heading>Instructional Design</template>
 
@@ -25,7 +25,7 @@ Flyers, Logos, Banners, Social Media Posts, and more. I provide professional gra
 
   <WelcomeItem>
     <template #icon>
-      <i class="fi fi-tc-paintbrush-pencil"></i>
+      <i class="fi fi-sr-paintbrush-pencil"></i>
     </template>
     <template #heading>Training Development</template>
 
@@ -34,7 +34,7 @@ Flyers, Logos, Banners, Social Media Posts, and more. I provide professional gra
 
   <WelcomeItem>
     <template #icon>
-   <i class="fi fi-ts-chart-histogram"></i>
+ <i class="fi fi-sr-growth-chart-invest"></i>
     </template>
     <template #heading>SAP Training</template>
 
